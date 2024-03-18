b5201bd5c6fd59db4ce41bbed1c6563df46b9de1
Logeshwaran29 <logeshwarancse20@gmail.com>
2024-03-18 22:40:49 +0530

    new

--- client/src/components/chat.jsx
+++ client/src/components/chat.jsx
@@ -2,7 +2,7 @@ import React, { useState , useEffect , useRef } from 'react';
 import './chat.css';
 import axios from 'axios';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
+import logo from './images/logo.png';
 
 const UserMessage = ({ text }) => {
   const content = Object.values(text).map((value, index) => (
@@ -17,6 +17,7 @@ const BotMessage = ({ text }) => {
   ));
   return <div className="bot">{content}</div>
 };
+
 
 const Chat = () => {
   const [input, setInput] = useState('');
@@ -71,7 +72,7 @@ const Chat = () => {
               <div className='user-msg'>
                 <div className='user-icon'>
                   <svg xmlns="http://www.w3.org/2000/svg" height={30} width={30} viewBox="0 0 512 512">
-                    <path fill='#b366ff' d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>                  
+                    <path fill='#2D68C4' d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>                  
                   </svg>
                 </div>
                 <div className='user-msg-div'>
@@ -81,7 +82,9 @@ const Chat = () => {
               </div>
                 ) : (
                   <div className="user-msg">
-                    <div className="bot-icon"></div>
+                    <div className="bot-icon">
+                      <img src={logo} alt="logo"/>
+                    </div>
                     <div className="user-msg-div">
                       <label><b>ChatWhiz</b></label>
                       <BotMessage key={index} text={message.text} />
